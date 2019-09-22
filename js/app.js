df76dd246bfe160e0514f3003c5d5a82937ec9bc
Ehsan Ghafari <ehsanghafari88@gmail.com>
2019-09-22 13:22:04 -0700

    fix some codes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
 'use strict';
 
-// get elements
-
 var resultEl = document.getElementById('results');
-
 var containerEl = document.getElementById('container');
-
-var filepathArr = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
-
-var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-
 var numberOfImg = 3;
 var totalClick = 0;
 var maxClick = 25;
 var indexArr = [];
 var allItems = [];
+var filepathArr = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 
-// construction function
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 function Product (name, pathway){
   this.name = name;
   this.pathway = pathway;
@@ -26,7 +20,6 @@ function Product (name, pathway){
   allItems.push(this);
 }
 
-// helper function
 function addElement(element, content, parent){
   var newElement = document.createElement(element);
   if(content){
@@ -38,7 +31,6 @@ function addElement(element, content, parent){
   return newElement;
 }
 
-// creating image elements
 Product.prototype.renderImage = function(parent){
   var imageEl = document.createElement('img');
   imageEl.src = this.pathway;
@@ -49,12 +41,10 @@ Product.prototype.renderImage = function(parent){
   return imageEl;
 };
 
-// helper function - calculate randon number
 function random(max){
   return Math.floor(Math.random() * (max));
 }
 
-// creating a unick index for 6 images
 function createIndex() {
   var index = random(allItems.length);
   while (indexArr.includes(index)) {
@@ -67,7 +57,6 @@ function createIndex() {
   return index;
 }
 
-// generating images
 function generatePicture() {
   var index = createIndex();
   var newEl = allItems[index].renderImage(containerEl);
@@ -75,7 +64,6 @@ function generatePicture() {
   return newEl;
 }
 
-// rendering pictures to the page
 function renderAllPictures() {
   while(containerEl.firstChild) {
     containerEl.removeChild(containerEl.firstChild);
@@ -85,7 +73,6 @@ function renderAllPictures() {
   }
 }
 
-// event handler
 function eventClick(e) {
   totalClick++;
   for (var i = 0; i < allItems.length; i++) {
@@ -100,26 +87,21 @@ function eventClick(e) {
   renderAllPictures();
 }
 
-// envoking this function after 25 votes
 function addResult(){
-  addElement('h3', 'Results', resultEl);
+  addElement('h3', 'You got it!', resultEl);
   var ulEl = addElement ('ul', false, resultEl);
   for (var i = 0; i < allItems.length; i++) {
     addElement('li', textContent, ulEl);
-    var textContent = allItems[i].name + ' ' + allItems[i].views + ' views ' + allItems[i].votes + ' votes;';
+    var textContent = allItems[i].name + ' got ' + allItems[i].views + ' views and ' + allItems[i].votes + ' votes;';
   }
 }
 
-
-// creating new image instance
 for (var i = 0; i < filepathArr.length; i++) {
   new Product(names[i],filepathArr[i]);
 }
 
-// render all pictures
 renderAllPictures();
 
-// adding event listenar
 containerEl.addEventListener('click', eventClick);
 
 
